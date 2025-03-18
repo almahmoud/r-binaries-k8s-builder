132fb0b0eaea3394d4f2e3dfec6cff383633def4
Alexandru Mahmoud <almahmoudresearch@gmail.com>
2025-03-18 18:05:08 -0400

    Syntax

--- .github/scripts/handle_k8s_jobs.sh
+++ .github/scripts/handle_k8s_jobs.sh
@@ -68,7 +68,7 @@ echo "${JOBS}" | while read -r JOB_NAME PKG; do
         fi
 
         # Determine final status
-        if [ "$SUCCEEDED" = "True" ] && [ $TARBALL_EXISTS -eq 1]; then
+        if [ "$SUCCEEDED" = "True" ] && [ $TARBALL_EXISTS -eq 1 ]; then
             mv "${TMP_LOG}" "${LOG_DIR}/build-success.log"
             echo "  Build succeeded with verified packaging"
             echo "$PKG" >> "${SUCCESS_PKGS}"
